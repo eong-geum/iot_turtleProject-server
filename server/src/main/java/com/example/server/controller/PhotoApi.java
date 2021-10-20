d43e58465dccce4e2e8228edaff5e0d33ca34783
beomseok <qjatjr29@khu.ac.kr>
2021-10-20 22:14:33 +0900

    Modify POST in JSON format

--- server/src/main/java/com/example/server/controller/PhotoApi.java
+++ server/src/main/java/com/example/server/controller/PhotoApi.java
@@ -21,54 +21,33 @@ public class PhotoApi {
 
     private PhotoDto photo=new PhotoDto();
 
+    // GET
     @GetMapping("/photo")
     public String getPhoto(){
-        System.out.println("get >>> "+ photo);
-        JsonObject jsonObject=new JsonObject();
-        jsonObject.addProperty("name",photo.getName());
-        jsonObject.addProperty("encodingContent",photo.getEncodingContent());
-        return jsonObject.toString();
+        return photoApiService.getData();
+//        System.out.println("get >>> "+ photo);
+//        JsonObject jsonObject=new JsonObject();
+//        jsonObject.addProperty("name",photo.getName());
+//        jsonObject.addProperty("encodingContent",photo.getEncodingContent());
+//        return jsonObject.toString();
     }
 
     //raspberryPI photo encoding -> spring loadbalancer -> opencv worker
     @PostMapping("/photo")
     public void postPhoto(@RequestBody PhotoDto photoDto){
-        System.out.println("post >>>" + photoDto);
-        photo.setName(photoDto.getName());
-        photo.setEncodingContent(photoDto.getEncodingContent());
 
-        //  ========send to worker=========
+        System.out.println("POST API");
+        // POST - raspberryPI photo encoding data
+        // set encoding data
+        photoApiService.setData(photoDto);
 
-        // URL 설정
-        String workerIP = "localhost";
-        String workerPort = "5000";
-        String method = "/opencv";
-        String url = "http://"+workerIP+":"+workerPort+method;
+        // is it a turtle-neck?
+        String isTurtle = photoApiService.getResult();
 
-        // Body 설정
-        MultiValueMap<String, String> params= new LinkedMultiValueMap<>();
-        params.add("name", photo.getName());
-        params.add("encodingContent", photo.getEncodingContent());
-
-        // Header 설정
-        HttpHeaders headers = new HttpHeaders();
-
-        // 요청하기 위해 header와 body 합치기
-        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params, headers);
-
-        // POST 요청
-        RestTemplate template = new RestTemplate();
-        ResponseEntity<String> response = template.exchange(
-                url,
-                HttpMethod.POST,
-                entity,
-                String.class
-        );
-        System.out.println("response = " + response);
         /*
-        if(response.getBody() == "turtle"){
+        if(isTurtle == "turtle"){
 
-        }else if(response.getBody() == "normal"){
+        }else if(isTurtle == "normal"){
 
         }*/
 
