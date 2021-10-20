8e1bb98f06fb19b7902bac94a41872b785b53a9a
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-10-20 16:06:12 +0900

    raspberry->spring->worker->spring flows successfully.

--- front/worker/main.py
+++ front/worker/main.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, request, jsonify, Response
+import json
 
 app = Flask(__name__)
 
@@ -6,6 +7,14 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/opencv', methods=['POST'])
+def opencv():
+    data = request.get_json()
+    print(data)
+    response = Response(status=200)
+    return response
 
 
 if __name__ == "__main__":
--- front/worker/photo_decoding.py
+++ front/worker/photo_decoding.py
@@ -3,12 +3,16 @@ import requests
 import json
 import cv2
 
-res = requests.get("http://localhost:8080/photo")
-json_data = json.loads(res.content)
+# 아직 수정 필요
 
-# api에서 가져온 JSON data decode
-photo_decode = base64.b64decode(json_data["encodingContent"])
 
-# 'test.jpg' 파일로 사진 저장.
-with open('test.jpg', 'wb') as f_output:
-    f_output.write(photo_decode)
+def decode_image(image_name):
+    res = requests.get("http://localhost:8080/photo")
+    json_data = json.loads(res.content)
+
+    # api에서 가져온 JSON data decode
+    photo_decode = base64.b64decode(json_data["encodingContent"])
+
+    # 'test.jpg' 파일로 사진 저장.
+    with open(image_name, 'wb') as f_output:
+        f_output.write(photo_decode)
