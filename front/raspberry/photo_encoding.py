ecf77048e70a0a7bf6ea01093d5403ed52323936
Kangho-Noh <rkdgh98@khu.ac.kr>
2021-10-20 15:45:27 +0900

    worker on port 5000

--- front/raspberry/photo_encoding.py
+++ front/raspberry/photo_encoding.py
@@ -6,15 +6,14 @@ import json
 from requests.api import head
 
 
-# # image file 
-# image='C:/Users/beomsic/Desktop/bg.jpeg' 
+# # image file
+# image='C:/Users/beomsic/Desktop/bg.jpeg'
 
 # encode_image=""
 # # print(type(encode_image))
 # with open(image,'rb') as img:
 #     img_b64=base64.b64encode(img.read())
 #     encode_image=bytes.decode(img_b64)
-
 
 
 # data={"name":"client","encoingContent":encode_image}
@@ -25,25 +24,25 @@ from requests.api import head
 # res=requests.post("http://172.10.8.60:8080/photo",json={'name':'client','encodingContent':encode_image},headers=headers)
 
 
-
 # 이미지 읽어오기
 def loadImage(file_path):
-    src=cv2.imread(file_path,cv2.IMREAD_COLOR)
+    src = cv2.imread(file_path, cv2.IMREAD_COLOR)
     # 이미지 인코딩
-    retval, buf=cv2.imencode('.jpg',src)
-    # base64 로 text로 인코딩    
-    jpg_as_text=bytes.decode(base64.b64encode(buf))
-    
-    return src,retval,buf,jpg_as_text
+    retval, buf = cv2.imencode('.jpg', src)
+    # base64 로 text로 인코딩
+    jpg_as_text = bytes.decode(base64.b64encode(buf))
 
-src,retval,buf,jpg_as_text=loadImage('C:/Users/beomsic/Desktop/bg.jpeg')
-
-# JSON 형식으로 서버에 전달
-headers={'Content-Type':'application/json; charset=utf-8'}
-res=requests.post("http://172.10.8.60:8080/photo",json={'name':'client','encodingContent':jpg_as_text},headers=headers)
+    return src, retval, buf, jpg_as_text
 
 
+if __name__ == "__main__":
+    image_path = "./normal.jpeg"
+    src, retval, buf, jpg_as_text = loadImage(image_path)
 
+    # JSON 형식으로 서버에 전달
+    headers = {'Content-Type': 'application/json; charset=utf-8'}
+    res = requests.post("http://localhost:8080/photo",
+                        json={'name': 'client', 'encodingContent': jpg_as_text}, headers=headers)
 
 
 # # 원본, 인코딩, 디코딩 이미지 화면 출력
@@ -54,4 +53,3 @@ res=requests.post("http://172.10.8.60:8080/photo",json={'name':'client','encodin
 # 화면 출력창 대기/닫기
 # cv2.waitKey()
 # cv2.destroyAllWindows()
-
--- server/src/main/java/com/example/server/controller/PhotoApi.java
+++ server/src/main/java/com/example/server/controller/PhotoApi.java
@@ -2,10 +2,14 @@ package com.example.server.controller;
 
 import com.example.server.dto.PhotoDto;
 import com.google.gson.JsonObject;
+import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class PhotoApi {
@@ -21,10 +25,47 @@ public class PhotoApi {
         return jsonObject.toString();
     }
 
+    //raspberryPI photo encoding -> spring loadbalancer -> opencv worker
     @PostMapping("/photo")
     public void postPhoto(@RequestBody PhotoDto photoDto){
         System.out.println("post >>>" + photoDto);
         photo.setName(photoDto.getName());
         photo.setEncodingContent(photoDto.getEncodingContent());
+
+        //  ========send to worker=========
+
+        // URL 설정
+        String workerIP = "localhost";
+        String workerPort = "5000";
+        String method = "/opencv";
+        String url = "http://"+workerIP+":"+workerPort+method;
+
+        // Body 설정
+        MultiValueMap<String, String> params= new LinkedMultiValueMap<>();
+        params.add("name", photo.getName());
+        params.add("encodingContent", photo.getEncodingContent());
+
+        // Header 설정
+        HttpHeaders headers = new HttpHeaders();
+
+        // 요청하기 위해 header와 body 합치기
+        HttpEntity<MultiValueMap<String, String>> entity = new HttpEntity<>(params, headers);
+
+        // POST 요청
+        RestTemplate template = new RestTemplate();
+        ResponseEntity<String> response = template.exchange(
+                url,
+                HttpMethod.POST,
+                entity,
+                String.class
+        );
+        System.out.println("response = " + response);
+        /*
+        if(response.getBody() == "turtle"){
+
+        }else if(response.getBody() == "normal"){
+
+        }*/
+
     }
 }
